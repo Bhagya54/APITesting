afb6ece9e7949b64443abd7f4a4ece77811af0e2
Bhagya54 <testingtrainer54@gmail.com>
2026-01-05 13:14:30 +0530

    By X on branch1

--- src/test/java/day2_BDDStyle_HTTPMethods/sample.java
+++ src/test/java/day2_BDDStyle_HTTPMethods/sample.java
@@ -3,5 +3,8 @@ package day2_BDDStyle_HTTPMethods;
 public class sample {
     public static void main(String[] args) {
         System.out.println("Added by Y");
+        System.out.println("In Branch1_EST_Added by X");
     }
+
+
 }
